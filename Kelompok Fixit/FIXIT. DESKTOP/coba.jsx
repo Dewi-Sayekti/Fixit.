01e4af22267274a7d049023f0d14a2af2ef4f670
Dewi-Sayekti <dsutrisni037@gmail.com>
2026-03-16 20:12:32 +0700

    Add files via upload

---
+++ Kelompok Fixit/FIXIT. DESKTOP/coba.jsx
@@ -0,0 +1,61 @@
+<div style={{width: 1440, height: 1024, position: 'relative', background: 'white', overflow: 'hidden'}}>
+  <div style={{width: 1440, height: 1023, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #213158 100%)', overflow: 'hidden', outline: '8px var(--Schemes-Outline-Variant, #CAC4D0) solid'}}>
+    <div data-camera-cutout="true" style={{width: 1440, height: 52, paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, left: 0, top: 0, position: 'absolute', justifyContent: 'space-between', alignItems: 'flex-end', display: 'inline-flex'}}>
+      <div style={{justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'var(--Schemes-On-Surface, #1D1B20)', fontSize: 14, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 20, letterSpacing: 0.14, wordWrap: 'break-word'}}>9:30</div>
+      <div style={{width: 17, height: 17}} />
+      <div style={{width: 17, height: 17}} />
+      <div style={{width: 17, height: 17}} />
+      <div style={{width: 17, height: 14.17, opacity: 0.10, background: 'var(--Schemes-On-Surface, #1D1B20)'}} />
+      <div style={{width: 17, height: 17}} />
+      <div style={{width: 14.17, height: 14.17, background: 'var(--Schemes-On-Surface, #1D1B20)'}} />
+      <div style={{width: 8, height: 15, position: 'relative'}}>
+        <div style={{width: 8, height: 15, left: 0, top: 0, position: 'absolute', opacity: 0.30, background: 'var(--Schemes-On-Surface, #1D1B20)'}} />
+        <div style={{width: 8, height: 7, left: 0, top: 8, position: 'absolute', background: 'var(--Schemes-On-Surface, #1D1B20)'}} />
+      </div>
+      <div style={{width: 24, height: 24, left: 708, top: 18, position: 'absolute', background: 'var(--Schemes-On-Surface, #1D1B20)'}} />
+    </div>
+    <div style={{width: 1440, height: 24, left: 0, top: 999, position: 'absolute'}}>
+      <div style={{width: 108, height: 4, left: 666, top: 10, position: 'absolute', background: 'var(--Schemes-On-Surface, #1D1B20)', borderRadius: 12}} />
+    </div>
+  </div>
+  <div style={{width: 1070, height: 775, left: 370, top: 248, position: 'absolute'}}>
+    <div style={{width: 161.33, height: 178.27, left: 908.67, top: 0, position: 'absolute', opacity: 0.24, background: 'rgba(255, 255, 255, 0.13)', boxShadow: '100px 100px 100px ', borderRadius: 9999, filter: 'blur(50px)'}} />
+  </div>
+  <img style={{width: 1070, height: 860, left: 370, top: 149, position: 'absolute'}} src="https://placehold.co/1070x860" />
+  <div style={{width: 370, height: 1023, left: 0, top: 1, position: 'absolute', background: 'black'}} />
+  <div style={{left: 39, top: 94, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 56, fontFamily: 'Brigends Expanded', fontWeight: '400', lineHeight: 20, letterSpacing: 0.56, wordWrap: 'break-word'}}>Fixit.</div>
+  <div style={{left: 43, top: 183, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>MENU</div>
+  <div style={{left: 111, top: 266, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>Beranda</div>
+  <div style={{left: 111, top: 348, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>Jelajah</div>
+  <div style={{left: 111, top: 473, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>Buat aduan</div>
+  <div style={{left: 111, top: 520, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>Proses</div>
+  <div style={{left: 111, top: 568, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>Riwayat</div>
+  <div style={{left: 111, top: 919, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>User Name</div>
+  <div style={{width: 142, height: 18, left: 111, top: 430, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 26, fontFamily: 'Roboto', fontWeight: '500', lineHeight: 18, letterSpacing: 0.26, wordWrap: 'break-word'}}>Pengaduan</div>
+  <div data-size="48" style={{width: 48, height: 48, left: 25, top: 251, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 36, height: 40, left: 6, top: 4, position: 'absolute', outline: '4px var(--Icon-Brand-On-Brand, #F5F5F5) solid', outlineOffset: '-2px'}} />
+  </div>
+  <div style={{width: 48, height: 48, left: 29, top: 333, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 36, height: 36, left: 6, top: 6, position: 'absolute', outline: '2.50px var(--Icon-Brand-On-Brand, #F5F5F5) solid', outlineOffset: '-1.25px'}} />
+  </div>
+  <div data-size="48" style={{width: 48, height: 48, left: 25, top: 415, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 40, height: 36, left: 4, top: 6, position: 'absolute', outline: '4px var(--Icon-Neutral-On-Neutral, #F3F3F3) solid', outlineOffset: '-2px'}} />
+  </div>
+  <div style={{width: 18, height: 18, left: 270, top: 430, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 10.50, height: 10.50, left: 3.75, top: 3.75, position: 'absolute', outline: '1.60px var(--Icon-Brand-On-Brand, #F5F5F5) solid', outlineOffset: '-0.80px'}} />
+  </div>
+  <div style={{width: 121, height: 0, left: 77, top: 468, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: 'top left', outline: '1px white solid', outlineOffset: '-0.50px'}}></div>
+  <div style={{width: 102, height: 16, left: 385, top: 82, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 31, fontFamily: 'Graphite DEMO', fontWeight: '400', lineHeight: 18, letterSpacing: 0.31, wordWrap: 'break-word'}}>Area</div>
+  <div style={{width: 48, height: 48, left: 400, top: 154, position: 'absolute', background: '#D9D9D9'}} />
+  <div data-has-icon-end="false" data-has-icon-start="false" data-size="Medium" data-state="Default" data-variant="Primary" style={{width: 138, padding: 12, left: 1286, top: 87, position: 'absolute', background: 'var(--Background-Neutral-Secondary-Hover, #B2B2B2)', overflow: 'hidden', borderRadius: 8, outline: '1px var(--Border-Neutral-Secondary, #767676) solid', outlineOffset: '-1px', justifyContent: 'center', alignItems: 'center', gap: 8, display: 'inline-flex'}}>
+    <div style={{color: 'var(--Text-Brand-On-Brand-Secondary, #1E1E1E)', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16, wordWrap: 'break-word'}}>Lantai 1  ></div>
+  </div>
+  <div data-has-icon-end="false" data-has-icon-start="false" data-size="Medium" data-state="Default" data-variant="Primary" style={{width: 138, height: 76, padding: 12, left: 1298, top: 128, position: 'absolute', background: 'var(--Background-Neutral-Tertiary, #E3E3E3)', borderRadius: 8, outline: '1px var(--Border-Default-Default, #D9D9D9) solid', outlineOffset: '-1px', justifyContent: 'center', alignItems: 'center', gap: 8, display: 'inline-flex'}} />
+  <div data-size="48" style={{width: 48, height: 48, left: 402, top: 154, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 32, height: 32, left: 8, top: 8, position: 'absolute', outline: '4px var(--Icon-Default-Default, #1E1E1E) solid', outlineOffset: '-2px'}} />
+  </div>
+  <div style={{left: 1326, top: 132, position: 'absolute'}}><span style="color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16, wordWrap: 'break-word'">Pilihan :<br/></span><span style="color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16, wordWrap: 'break-word'">Lobby <br/>Lantai 2<br/>Lantai 1</span></div>
+  <div style={{width: 48, height: 48, left: 31, top: 899, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 32, height: 36, left: 8, top: 6, position: 'absolute', outline: '1.60px var(--Icon-Brand-On-Brand, #F5F5F5) solid', outlineOffset: '-0.80px'}} />
+  </div>
+</div>
