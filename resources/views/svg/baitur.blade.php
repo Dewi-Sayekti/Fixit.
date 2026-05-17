979801ef658a0602c3b71ba460f393130f24a1f6
Dewi-Sayekti <dsutrisni037@gmail.com>
2026-05-17 15:47:45 +0700

    Add files via upload

---
+++ resources/views/svg/baitur.blade.php
@@ -0,0 +1,43 @@
+<svg width="739" height="853" viewBox="0 0 739 853" fill="none" xmlns="http://www.w3.org/2000/svg">
+<style>
+    .zona {
+        fill: transparent;
+        cursor: pointer;
+        transition: all 0.2s ease;
+    }
+    
+    .zona:hover {
+        fill: rgba(255,255,255,0.15);
+        stroke: #ffffff;
+        stroke-width: 2;
+        transform: scale(1.004);
+    }
+    
+    .zona.selected {
+        fill: rgba(255,255,255,0.25);
+        stroke: #ffffff;
+        stroke-width: 3;
+        filter: drop-shadow(0 0 8px white);
+    }
+</style>
+<g id="baitur">
+<g id="kamarbesar" class="zona" data-zona-id="1" data-nama="Kamar Besar"><path id="kamar besar" d="M738 278V851.938L553 830.056V278H738Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="parkirspace" class="zona" data-zona-id="2" data-nama="Parkir Space"><path id="parkir space" d="M552 153.333V277H193V42.9346L192.439 43.0039L136 49.9346V11.8975L184.956 0.523438L552 153.333Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="jamaahpria" class="zona" data-zona-id="3" data-nama="Jamaah Pria"><path id="jamaah pria" d="M460 350V758H236V586.5H193V563.5H236V500H193V350H460Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="gedungsamping" class="zona" data-zona-id="4" data-nama="Gedung Samping"><path id="gedung samping" d="M192 350V758H65.4268L0.585938 350H192Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="asramaparkirspace" class="zona" data-zona-id="5" data-nama="Asrama Parkir Space"><path id="asrama parkir space" d="M135.5 350H2V38.9121L135.5 13.1055V350Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="serambikanan" class="zona" data-zona-id="6" data-nama="Serambi Kanan"><rect id="serambi kanan" x="193" y="278" width="359" height="71" fill="#D9D9D9" stroke="black"/></g>
+<g id="jamaahperempuan" class="zona" data-zona-id="7" data-nama="Jamaah Perempuan"><path id="jamaah perempuan" d="M552 351V761H461V351H552Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="serambikiri" class="zona" data-zona-id="8" data-nama="Serambi Kiri"><rect id="serambi kiri" x="193" y="759" width="359" height="71" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamarmandiwanita" class="zona" data-zona-id="9" data-nama="Kamar Mandi Wanita"><path id="kamar mandi wanita" d="M192 204V349H136V204H192Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamarmandipria" class="zona" data-zona-id="10" data-nama="Kamar Mandi Pria"><path id="kamar mandi pria" d="M192 203H136V49.2305L192 43.0576V203Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="imamspace" class="zona" data-zona-id="11" data-nama="Imam Space"><rect id="imam space" x="193" y="501" width="42" height="62" fill="#D9D9D9" stroke="black"/></g>
+<g id="ambulanceport" class="zona" data-zona-id="12" data-nama="Ambulance Port"><path id="ambulance port" d="M135 117H93.0195L94.9854 64.8955L135 55.1357V117Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="ruangmuadzin" class="zona" data-zona-id="13" data-nama="Ruang Muadzin"><rect id="ruang muadzin" x="193" y="587" width="42" height="170" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamarmandisantri" class="zona" data-zona-id="14" data-nama="Kamar Mandi Santri"><rect id="kamar mandi santri" x="65" y="587" width="56" height="170" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamar2" class="zona" data-zona-id="15" data-nama="Kamar 2"><rect id="kamar 2" x="136" y="507" width="56" height="117" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamar3" class="zona" data-zona-id="16" data-nama="Kamar 3"><rect id="kamar 3" x="136" y="372" width="56" height="117" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamarustad" class="zona" data-zona-id="17" data-nama="Kamar Ustad"><path id="kamar ustad" d="M118 351V407H9.42969L1.0791 351H118Z" fill="#D9D9D9" stroke="black"/></g>
+<g id="kamar1" class="zona" data-zona-id="18" data-nama="Kamar 1"><rect id="kamar 1" x="136" y="640" width="56" height="117" fill="#D9D9D9" stroke="black"/></g>
+</g>
+</svg>
